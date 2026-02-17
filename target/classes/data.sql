eebcd51bd3345b7c12d821eb1979e42e777ed04e
Themba Ubisi <reginalthembaubisi@gmail.com>
2026-02-17 15:27:29 +0200

    chore: cleanup gitignore and remove build artifacts

--- target/classes/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO users (username, password, role)
-SELECT 'admin', '$2b$10$sZ1uAVGP1sUw.QvnawfxyuUe9Jw9um00/I0KrmGXQIUmwXPE/aEu6', 'ADMIN'
-WHERE NOT EXISTS (SELECT 1 FROM users WHERE username = 'admin');
